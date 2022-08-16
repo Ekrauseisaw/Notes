07f4a0a69b7e5aca7af8d25b82be46fad15859b2
Ekrause <ekrauseisaw@yandex.ru>
2022-08-16 12:18:55 +1000

    Added models: Note, Content for notices app.

--- notices/admin.py
+++ notices/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from notices.models import Note, Content
 
 # Register your models here.
+
+admin.site.register(Note)
+admin.site.register(Content)
--- notices/models.py
+++ notices/models.py
@@ -1,4 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+class Note(models.Model):
+    """ Note title. """
+    text = models.CharField(max_length=600)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """return string model"""
+        return self.text
+
+
+class Content(models.Model):
+    """Content of note."""
+    note = models.ForeignKey(Note, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    # class Meta
+
+    def __str__(self):
+        if self.text[50:]:
+            return self.text[:50] + "..."
+        else:
+            return self.text[:50]
